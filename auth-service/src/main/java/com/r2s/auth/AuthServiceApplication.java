ab99623aa2c8174901b8d6e51a137f26c8afdf4d
ngoc son <gowahskoja113@gmail.com>
2026-03-12 14:28:43 +0700

    add(auth): add spring boot actuator, prometheus, logback, monitoring, logging

--- auth-service/src/main/java/com/r2s/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/r2s/auth/AuthServiceApplication.java
@@ -1,5 +1,6 @@
 package com.r2s.auth;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -13,6 +14,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "com.r2s.core.entity")
 public class AuthServiceApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 }
--- auth-service/src/main/java/com/r2s/auth/config/SecurityConfig.java
+++ auth-service/src/main/java/com/r2s/auth/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
